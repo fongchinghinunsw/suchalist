1b60a234d51aa78c4c68531a13f9c38565856da4
Stephen Fong <fongchinghinunsw@gmail.com>
2025-04-29 22:53:09 +1000

    Fix time format

--- suchalist-mobile/src/utils/format.ts
+++ suchalist-mobile/src/utils/format.ts
@@ -5,11 +5,26 @@ type DateFormat = 'datetime' | 'date' | 'time';
 export const formatDate = (date: Date, format: DateFormat) => {
   switch (format) {
     case 'datetime':
-      return date.toLocaleString();
+      return date.toLocaleString(undefined, {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true, // or false for 24-hour format
+      });
     case 'date':
-      return date.toLocaleDateString();
+      return date.toLocaleDateString(undefined, {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      });
     case 'time':
-      return date.toLocaleTimeString();
+      return date.toLocaleTimeString(undefined, {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+      });
     default:
       throw new UnreachableError(format);
   }
