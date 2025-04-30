7575a52ff02793cea0a87eb330901f505e061f0b
Stephen Fong <fongchinghinunsw@gmail.com>
2025-04-30 19:31:43 +1000

    Make light theme

--- suchalist-mobile/src/App.tsx
+++ suchalist-mobile/src/App.tsx
@@ -34,7 +34,7 @@ export default function App(): React.JSX.Element {
   return (
     <ReduxStoreProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <PaperProvider>
+        <PaperProvider theme={{dark: false}}>
           <GestureHandlerRootView>
             <BottomSheetModalProvider>
               <NavigationContainer>
