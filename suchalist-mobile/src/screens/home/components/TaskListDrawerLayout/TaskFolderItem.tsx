a05092670e11f1a7ea7b7aedbc560cb0ca8cdc9f
Stephen Fong <fongchinghinunsw@gmail.com>
2025-05-11 14:43:44 +1000

    Add toggle

--- suchalist-mobile/src/screens/home/components/TaskListDrawerLayout/TaskFolderItem.tsx
+++ suchalist-mobile/src/screens/home/components/TaskListDrawerLayout/TaskFolderItem.tsx
@@ -5,6 +5,7 @@ import Animated from 'react-native-reanimated';
 import {FolderHeader} from './types';
 import {useSelector} from 'react-redux';
 import {selectListsMap} from '@/stores/tasks/tasks';
+import {useState} from 'react';
 
 type Props = {
   folderHeader: FolderHeader;
@@ -17,42 +18,68 @@ export default function TaskFolderItem({
 }: Props) {
   const listsMap = useSelector(selectListsMap);
 
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const onToggleListItem = () => {
-    console.log('toggle');
+    setIsExpanded(!isExpanded);
   };
+
+  const folderItemIconName = isExpanded
+    ? 'chevron-down-outline'
+    : 'chevron-back-outline';
 
   return (
     <Animated.View>
       <Pressable style={styles.container} onPress={onToggleListItem}>
-        <Icon name="folder-open-outline" size={16} />
-        <Text size="large">{title}</Text>
+        <View style={styles.innerContainer}>
+          <View style={styles.titleContainer}>
+            <Icon name="folder-open-outline" size={16} />
+            <Text size="large">{title}</Text>
+          </View>
+          <Icon name={folderItemIconName} size={16} />
+        </View>
       </Pressable>
-      <View style={styles.listsContainer}>
-        {lists.map(list => {
-          const listItem = listsMap[list];
-          return (
-            <Pressable
-              key={listItem.id}
-              style={styles.container}
-              onPress={() => onPress(listItem.id)}>
-              <Icon name="list-outline" size={16} />
-              <Text size="large">{listItem.title}</Text>
-            </Pressable>
-          );
-        })}
-      </View>
+      {isExpanded && (
+        <View style={styles.listsContainer}>
+          {lists.map(list => {
+            const listItem = listsMap[list];
+            return (
+              <Pressable
+                key={listItem.id}
+                style={styles.listItemContainer}
+                onPress={() => onPress(listItem.id)}>
+                <Icon name="list-outline" size={16} />
+                <Text size="large">{listItem.title}</Text>
+              </Pressable>
+            );
+          })}
+        </View>
+      )}
     </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 12,
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+  },
+  listsContainer: {
+    paddingLeft: 12,
+  },
+  listItemContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 12,
     padding: 12,
-  },
-  listsContainer: {
-    paddingLeft: 12,
   },
 });
