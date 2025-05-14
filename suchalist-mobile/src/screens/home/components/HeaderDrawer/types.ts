e18c7aeda9dc31773be278f2cf34ff686cb46fe2
Stephen Fong <fongchinghinunsw@gmail.com>
2025-05-14 16:23:28 +1000

    Add jsdoc

--- suchalist-mobile/src/screens/home/components/HeaderDrawer/types.ts
+++ suchalist-mobile/src/screens/home/components/HeaderDrawer/types.ts
@@ -1,9 +1,25 @@
 import {Folder, List} from '@/stores/tasks/types';
 
-export type ListHeader = Pick<List, 'id' | 'title'> & {type: 'LIST'};
+/**
+ * Represents a list header item.
+ * @property {string} id - The unique identifier of the list.
+ * @property {string} title - The title of the list.
+ */
+export type ListHeader = {
+  type: 'LIST';
+} & Pick<List, 'id' | 'title'>;
 
-export type FolderHeader = Pick<Folder, 'id' | 'title' | 'lists'> & {
+/**
+ * Represents a folder header item which can contain multiple list header items.
+ * @property {string} id - The unique identifier of the folder.
+ * @property {string} title - The title of the folder.
+ * @property {string[]} lists - The array of lists' unique identifiers contained within the folder.
+ */
+export type FolderHeader = {
   type: 'FOLDER';
-};
+} & Pick<Folder, 'id' | 'title' | 'lists'>;
 
+/**
+ * Union type representing either a list header or a folder header.
+ */
 export type Header = ListHeader | FolderHeader;
