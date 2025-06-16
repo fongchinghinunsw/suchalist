0fb105c9dbcc5dafd53ea347f9d6584b819e9c3d
Stephen Fong <fongchinghinunsw@gmail.com>
2025-06-16 22:43:18 +1000

    Add plus

--- suchalist-desktop/src/renderer/src/components/Layout/SideBar/Footer.tsx
+++ suchalist-desktop/src/renderer/src/components/Layout/SideBar/Footer.tsx
@@ -1,5 +1,11 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import IconButton from '@renderer/components/base/IconButton';
-import { IoSettingsOutline } from 'react-icons/io5';
+import {
+  IoAddOutline,
+  IoFolderOpenOutline,
+  IoListOutline,
+  IoSettingsOutline
+} from 'react-icons/io5';
 import { useNavigate } from 'react-router';
 
 export default function Footer() {
@@ -10,7 +16,26 @@ export default function Footer() {
   };
 
   return (
-    <div className="fixed bottom-0 left-0 w-[25%] h-15 bg-gray-100 flex flex-row justify-end items-center overflow-auto px-4 py-2">
+    <div className="fixed bottom-0 left-0 w-[25%] h-15 bg-gray-100 flex flex-row justify-between items-center overflow-auto px-4 py-2">
+      <Menu>
+        <MenuButton>
+          <IconButton Icon={IoAddOutline} size={32} onClick={goToSettings} />
+        </MenuButton>
+        <MenuItems anchor="top start" className="border border-gray-400 rounded-lg p-2 bg-white">
+          <MenuItem>
+            <div className="flex items-center gap-2">
+              <IoListOutline />
+              <div>Add List</div>
+            </div>
+          </MenuItem>
+          <MenuItem>
+            <div className="flex items-center gap-2">
+              <IoFolderOpenOutline />
+              <div>Add Folder</div>
+            </div>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
       <IconButton Icon={IoSettingsOutline} size={32} onClick={goToSettings} />
     </div>
   );
